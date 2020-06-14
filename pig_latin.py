43beee2bf5e80896be786341b4a5b8e16bc9085a
Johnny <johnrussoofficial@outlook.com>
2020-06-14 14:32:24 -0400

    First commit

---
+++ pig_latin.py
@@ -0,0 +1,28 @@
+"""Change a whole sentence into pig latin!"""
+import sys
+
+VOWELS = "aeiouy"
+# more entries to be added to DO_NOT_CHANGE to be added if needed
+DO_NOT_CHANGE = ["on", "the", "a", "an", "if", "and"]
+
+while True:
+    sentence = input("Enter a sentence to convert to pig latin:\n")
+    words = sentence.split()
+    output = []
+    for word in words:
+        if word not in DO_NOT_CHANGE:
+            if word[0] in VOWELS:
+                new_word = word + 'way'
+                output.append(new_word)
+            else:
+                new_word = word[1: ] + word[0] + "ay"
+                output.append(new_word)
+        else:
+            output.append(word)
+    NEW_SENTANCE = " ".join(output)
+    print()
+    print("{}".format(NEW_SENTANCE), file=sys.stderr)
+
+    try_again = input("\n\nWant to try a new sentance? (Press Enter else 'n' to stop)\n")
+    if try_again.lower() == "n":
+        sys.exit()
