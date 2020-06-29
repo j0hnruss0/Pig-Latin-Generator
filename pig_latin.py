586a211816e9e4a241e8f86bf6dad013aabcdef3
Johnny <johnrussoofficial@outlook.com>
2020-06-29 18:19:21 -0400

    punctuation handling

--- pig_latin.py
+++ pig_latin.py
@@ -1,13 +1,19 @@
 """Change a whole sentence into pig latin!"""
 import sys
+import re
 
 VOWELS = "aeiouy"
 # more entries to be added to DO_NOT_CHANGE to be added if needed
 DO_NOT_CHANGE = ["on", "the", "a", "an", "if", "and", "are"]
+punc_list = ["?", "!", "."]
 
 while True:
-    sentence = input("Enter a sentence to convert to pig latin (No punctuation, please!):\n")
-    words = sentence.split()
+    sentence = input("Enter a sentence to convert to pig latin:\n")
+    punc_output = []
+    if sentence[-1] in punc_list:
+        punc_output.append(sentence[-1])
+    sentence_fix = re.sub(r'[^\w\s]','',sentence)
+    words = sentence_fix.split()
     output = []
     for word in words:
         if word not in DO_NOT_CHANGE:
@@ -21,7 +27,7 @@ while True:
             output.append(word)
     NEW_SENTANCE = " ".join(output)
     print()
-    print("{}".format(NEW_SENTANCE), file=sys.stderr)
+    print("{}{}".format(NEW_SENTANCE, "".join(punc_output)), file=sys.stderr)
 
     try_again = input("\n\nWant to try a new sentance? (Press Enter else 'n' to stop)\n")
     if try_again.lower() == "n":
